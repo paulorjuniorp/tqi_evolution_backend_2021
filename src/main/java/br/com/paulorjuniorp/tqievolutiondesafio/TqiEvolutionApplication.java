6a9324c799618f14272547338b2408c927535dd1
paulorjuniorp <paulorjuniorp@gmail.com>
2022-01-05 09:20:03 -0300

    Primeiro commit

---
+++ src/main/java/br/com/paulorjuniorp/tqievolutiondesafio/TqiEvolutionApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulorjuniorp.tqievolutiondesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqiEvolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqiEvolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/paulorjuniorp/tqievolutiondesafio/TqiEvolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulorjuniorp.tqievolutiondesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqiEvolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
